35880995926642891d21d101bdadc64d0044c931
Smotko <ensmotko@gmail.com>
2014-04-01 00:03:51 +0200

    Websockets!

--- web.js
+++ web.js
@@ -1,7 +1,10 @@
 var express = require("express")
   , logfmt = require("logfmt")
+  , http = require('http')
   , app = express()
   , logic = require('./logic.js')
+  , WebSocketServer = require('ws').Server
+  , port = process.env.PORT || 5000;
   ;
 
 // Set up the server
@@ -24,7 +27,27 @@ app.get('/check', function(req, res){
   });
 });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
+var server = http.createServer(app);
+server.listen(port);
+
+// Set up the websocket server
+var wss = new WebSocketServer({server: server});
+wss.on('connection', function(ws) {
+  
+  var last_created_time = 0
+    , id = setInterval(function() {
+      logic.retrieveLatest(function(latest){
+        if(!last_created_time){
+          last_created_time = parseInt(latest.created_time);
+        }
+        if(last_created_time < parseInt(latest.created_time)){
+          ws.send(JSON.stringify(latest), function() {  });
+          last_created_time = parseInt(latest.created_time);
+        }
+      });
+  }, 60000);
+
+  ws.on('close', function() {
+      clearInterval(id);
+  });
 });
