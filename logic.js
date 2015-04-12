9759a5435caa9d835e74a95902bde805c8b86fa7
Anže Pečar <ensmotko@gmail.com>
2015-04-12 10:54:35 +0200

    Don't fail on api error

--- logic.js
+++ logic.js
@@ -7,16 +7,16 @@ instagram.set('client_id', conf.instagram.client_id);
 instagram.set('client_secret', conf.instagram.client_secret);
 
 // Fetch recent user images and return the latest one
-exports.retrieveLatest = function(resolve){
+exports.retrieveLatest = function(resolve) {
 
   var results = [],
       returned_results = 0;
 
-  function check(){
+  function check() {
     if(returned_results !== user_ids.length) {
       return;
     }
-    results.sort(function(a,b){
+    results.sort(function (a,b) {
       return parseInt(b.created_time) - parseInt(a.created_time);
     });
     resolve(results);
@@ -26,10 +26,15 @@ exports.retrieveLatest = function(resolve){
     returned_results++;
     check();
   }
+  function error(r) {
+    returned_results++;
+    check();
+  }
   for (var i = user_ids.length - 1; i >= 0; i--) {
     instagram.users.recent({
       user_id: user_ids[i],
-      complete: complete
+      complete: complete,
+      error: error
     });
   }
 };
