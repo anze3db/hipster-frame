ba79360ecde8ec1ed8ae4df14321beabad51bced
Anže Pečar <ensmotko@gmail.com>
2016-08-04 01:20:34 +0200

    Check for authenticated session on the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const auth = document.cookie.split('; ').map((c) => c.substring(0, c.indexOf('='))).indexOf('auth') > -1;
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Hipster Frame 2!</h2>
         </div>
         <p className="App-intro">
-          Hello world
+          {auth && <a href="/api/instagram/logout">Logout</a>}
+          {!auth && <a href="/api/instagram/authorize">Authorize</a>}
         </p>
       </div>
     );
