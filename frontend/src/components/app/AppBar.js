98b2d9034a14753901ba04f480858cb3ba79a456
Anže Pečar <anze@pecar.me>
2016-10-25 00:00:58 +0200

    Move fullscreen functionality into own component

--- frontend/src/components/app/AppBar.js
+++ frontend/src/components/app/AppBar.js
@@ -3,35 +3,9 @@ import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import AppBar from 'material-ui/AppBar';
+import FullscreenButton from './FullscreenButton';
 
 class InstAppBar extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      isFullscreen: false
-    }
-  }
-  fullscreen() {
-    if (document.webkitIsFullScreen) {
-      document.webkitCancelFullScreen();
-    } else {
-      document.documentElement.webkitRequestFullScreen();
-    }
-  }
-
-  changeFullscreen() {
-    this.setState({isFullscreen: document.webkitIsFullScreen});
-  }
-
-  componentDidMount() {
-    document.onwebkitfullscreenchange = this.changeFullscreen.bind(this);
-  }
-
-  componentWillUnmount() {
-    document.onwebkitfullscreenchange = null;
-  }
-
   render() {
     return (
       <AppBar
@@ -39,12 +13,7 @@ class InstAppBar extends Component {
         iconElementLeft={<IconButton iconClassName="material-icons">photo_camera</IconButton>}
         iconElementRight={
           <div>
-            <IconButton
-              iconStyle={{color: "white"}}
-              tooltip="Fullscreen"
-              tooltipPosition="bottom-center"
-              onClick={this.fullscreen.bind(this)}
-              iconClassName="material-icons">{this.state.isFullscreen ? 'fullscreen_exit' : 'fullscreen'}</IconButton>
+            <FullscreenButton></FullscreenButton>
             <IconMenu
               anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
               targetOrigin={{horizontal: 'right', vertical: 'top'}}
---
+++ frontend/src/components/app/FullscreenButton.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import IconButton from 'material-ui/IconButton';
+
+class FullscreenButton extends Component {
+  // TODO: Support non webkit browsers
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFullscreen: false
+    }
+  }
+  fullscreen() {
+    if (document.webkitIsFullScreen) {
+      document.webkitCancelFullScreen();
+    } else {
+      document.documentElement.webkitRequestFullScreen();
+    }
+  }
+  changeFullscreen() {
+    this.setState({isFullscreen: document.webkitIsFullScreen});
+  }
+  componentDidMount() {
+    document.onwebkitfullscreenchange = this.changeFullscreen.bind(this);
+  }
+  componentWillUnmount() {
+    document.onwebkitfullscreenchange = null;
+  }
+  render() {
+    return (
+      <IconButton
+        iconStyle={{color: "white"}}
+        tooltip="Fullscreen"
+        tooltipPosition="bottom-center"
+        onClick={this.fullscreen.bind(this)}
+        iconClassName="material-icons">
+        {this.state.isFullscreen ? 'fullscreen_exit' : 'fullscreen'}
+      </IconButton>
+    );
+  }
+}
+
+export default FullscreenButton;
