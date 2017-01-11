720e4265450bc449b10b28b420229dd26a5bfd0f
Anže Pečar <anze@pecar.me>
2017-01-11 22:50:53 +0100

    Add test for splash page

---
+++ frontend/src/components/splash/Splash.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import theme from '../../theme.js';
+import Splash from './Splash';
+import { shallow } from 'enzyme';
+
+// Needed for onTouchTap
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+function renderSplash() {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <MuiThemeProvider muiTheme={theme}>
+      <Splash />
+    </MuiThemeProvider>, div);
+}
+
+it('renders splash without crashing', () => {
+  renderSplash();
+});
+
+it('should contain hipster-frame header', () => {
+  const splash = shallow(<Splash />);
+  expect(splash.contains(<h1>Hipster Frame</h1>)).toEqual(true);
+});
+
+it('should contain connect button', () => {
+  const splash = shallow(<Splash />);
+  expect(splash.find('RaisedButton')).toHaveLength(1);
+});
