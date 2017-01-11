e1585378f3b999877e2c0748b42a1ca2aeaec7f5
Anže Pečar <anze@pecar.me>
2017-01-11 22:50:13 +0100

    Shallow test App component

--- frontend/src/components/app/App.test.js
+++ frontend/src/components/app/App.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import theme from '../../theme.js';
 import App from './App';
+import { shallow } from 'enzyme';
 // Needed for onTouchTap
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
@@ -13,4 +14,11 @@ it('renders without crashing', () => {
     <MuiThemeProvider muiTheme={theme}>
       <App />
     </MuiThemeProvider>, div);
+});
+
+it('renders spinner, appbar & frame', () => {
+  const app = shallow(<App />);
+  expect(app.find('InstAppBar')).toHaveLength(1);
+  expect(app.find('Frame')).toHaveLength(1);
+  expect(app.find('RefreshIndicator')).toHaveLength(1);
 });
