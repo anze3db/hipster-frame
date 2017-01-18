7068cd410e3da7ce87318b29cc217f5f91f71db0
Anže Pečar <anze@pecar.me>
2017-01-19 00:28:19 +0100

    Add index test.test.js

---
+++ frontend/src/intex.test.js
@@ -0,0 +1,6 @@
+import './index.js';
+
+it('imports without a problem', () => {
+  // The index.js init method is a bit difficult to test
+  // so we just make sure it doesn't throw an error...
+});
