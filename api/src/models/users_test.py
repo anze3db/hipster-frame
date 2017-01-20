55fc2d6c001dfb0a18090b41ca9be739476d897c
Anže Pečar <anze@pecar.me>
2017-01-20 03:39:06 +0100

    Add user fixture

--- api/src/models/users_test.py
+++ api/src/models/users_test.py
@@ -9,10 +9,11 @@ class TestUsers(TestCase):
 
     def setUp(self):
         self.db = MagicMock()
+        with open('fixtures/user.fixture', 'r') as f:
+          self.json_str = f.read()
 
     def test_response_to_user(self):
-        json = u'{"user": {"username": "bond", "id": "007", "profile_picture": "https://scontent.cdninstagram.com/t51.2885-19/007_509223142569119_1064089302_a.jpg", "bio": "I\'m Bond. James Bond.", "full_name": "Jane\u017e Bondi\u010d", "website": "http://bond.si"}, "access_token": "00706441.c7943a3.2923c6a395ca42b99d6c7065eaaf57b7"}'
-        user = json_to_user(json)
+        user = json_to_user(self.json_str)
         assert user["id"] is None
         assert user["access_token"] == \
             "00706441.c7943a3.2923c6a395ca42b99d6c7065eaaf57b7"
