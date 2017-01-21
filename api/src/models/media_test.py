0678955b6ac95016bca74502f7f6c230c3823173
Anže Pečar <anze@pecar.me>
2017-01-21 19:40:51 +0100

    Add _get_args_str test

--- api/src/models/media_test.py
+++ api/src/models/media_test.py
@@ -19,6 +19,18 @@ def json_str():
         return f.read()
 
 
+def async_wrapper(*args):
+  async def _asyncmock():
+      a = MagicMock()
+      a.decode.return_value = "A"
+      return a
+  return _asyncmock()
+
+
+async def _asyncmock():
+    return MagicMock()
+
+
 def test_json_to_media(json_str):
     res = json_to_media(json_str, 1)
     assert len(res) > 0
@@ -32,13 +44,19 @@ def test_get_media(db):
     assert db.execute.called is True
 
 
-async def _asyncmock():
-    return MagicMock()
-
-
 @pytest.mark.asyncio
 async def test_insert_media():
     db = MagicMock()
     db.execute.return_value = _asyncmock()
     res = await insert_media(db, {})
     assert db.execute.call_count == 2
+
+
+@pytest.mark.asyncio
+async def test_get_args_str():
+    db = MagicMock()
+    db.mogrify.side_effect =  async_wrapper
+    data = [1, 2, 3]
+    values = {}
+    await _get_args_str(db, data, values)
+    assert db.mogrify.call_count == 3
